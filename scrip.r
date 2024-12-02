b0286174bb1b54052fe093440cbf54a8a18b7b1b
Matías <mnpizarroch@gmail.com>
2024-12-02 11:32:45 -0300

    commit 5

--- scrip.r
+++ scrip.r
@@ -3,4 +3,6 @@ git config --global user.email "mnpizarroch@gmail.com"
 git config --global user.name "Matías"
 ggplot()
 usethis::use_github()
-
+3
+usethis::gh_token_help() 
+gitcreds::gitcreds_set()
